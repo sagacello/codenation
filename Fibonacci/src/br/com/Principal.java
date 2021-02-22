e094563358e53fc0d116ca69c21eb7498522a0b6
marcelo ivan <sagacello@hotmail.com>
2021-02-22 14:43:26 -0300

    fibonacci feito de forma recursiva

---
+++ Fibonacci/src/br/com/Principal.java
@@ -0,0 +1,13 @@
+package br.com;
+
+import br.com.fibo.fibo;
+import br.com.fibo.fiboRecursiva;
+import br.com.fibo.fibopratica;
+
+
+public class Principal {
+    public static void main(String[] args) {
+         fiboRecursiva.MOSTRA();
+
+    }
+}
